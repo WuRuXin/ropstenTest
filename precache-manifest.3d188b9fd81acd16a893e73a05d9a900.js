5f39cf1915b0e0a4bb83980868555ee9830367d0
WuRuXin <1530484937@qq.com>
2022-05-06 11:21:51 +0800

    Updates

---
+++ precache-manifest.3d188b9fd81acd16a893e73a05d9a900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf680c974a43579092d4",
+    "url": "/ropstenTest/static/css/main.eb994cbc.chunk.css"
+  },
+  {
+    "revision": "cf680c974a43579092d4",
+    "url": "/ropstenTest/static/js/main.cf680c97.chunk.js"
+  },
+  {
+    "revision": "2f9cc80e42bc13b1d36e",
+    "url": "/ropstenTest/static/css/1.f8537aa9.chunk.css"
+  },
+  {
+    "revision": "2f9cc80e42bc13b1d36e",
+    "url": "/ropstenTest/static/js/1.2f9cc80e.chunk.js"
+  },
+  {
+    "revision": "991fc5e3d25fc5916cab",
+    "url": "/ropstenTest/static/js/runtime~main.991fc5e3.js"
+  },
+  {
+    "revision": "bcc35de769f68636abf0e401f113cf53",
+    "url": "/ropstenTest/index.html"
+  }
+];
